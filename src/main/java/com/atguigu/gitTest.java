aa3f2fb0b64a9fb80cfa81601163205f6b9fad1e
SQJ <1484851715@qq.com>
2021-03-26 23:26:49 +0800

    hot-fix4 commit by hot-fix

--- src/main/java/com/atguigu/gitTest.java
+++ src/main/java/com/atguigu/gitTest.java
@@ -7,5 +7,6 @@ public class gitTest {
         System.out.println("hello git3");
         System.out.println("hot-fix2");
         System.out.println("hot-fix3");
+        System.out.println("hot-fix4");
     }
 }
